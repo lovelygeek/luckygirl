33d001dcfb8fdde747dd57423b23daace348d27c
Cristina Robinson <cenamaria2787@gmail.com>
2016-09-23 06:56:27 -0700

    Uploaded site files

---
+++ luckygirl.php
@@ -0,0 +1,16 @@
+<div class="img-ilus" style="width: 300px;"><img src="http://cassenza.com/images/misc/luckygirl.jpg" alt="Lucky Girl" /></div>
+<strong>Made For:</strong> <a href="http://www.thefanlistings.org/">The Fanlistings</a>
+<strong>Featuring:</strong> <a href="http://www.amazon.com/Lucky-Brand-Women-Toilette-Spray/dp/B0007D3B56">Lucky Brand Fragrance for Women: Lucky You</a>
+<strong>Released:</strong> March 22nd, 2005
+<strong>Credits:</strong>
+	<ul>
+	<li>Fonts - <a href="http://en.wikipedia.org/wiki/Verdana">Tahoma</a>, <a href="http://www.urbanfonts.com/fonts/Saginaw.htm">Saginaw</a>, <a href="http://www.dsg4.com/04/extra/bitmap/">04b-03</a></li>
+	<li>Images - <a href="http://ulta.com">Ulta</a></li>
+	</ul>
+<strong>Notes:</strong> This design was made for the Lucky You Fragrance for Women fanlisting. It was retired in 2005 as I didn't have the time nor the desire to keep up with it.
+<strong>Buttons:</strong><br />
+<img src="http://cassenza.com/images/misc/luckygirl/button00.gif" alt="" />
+<img src="http://cassenza.com/images/misc/luckygirl/button01.gif" alt="" />
+<img src="http://cassenza.com/images/misc/luckygirl/button02.gif" alt="" />
+<img src="http://cassenza.com/images/misc/luckygirl/button03.gif" alt="" />
+<img src="http://cassenza.com/images/misc/luckygirl/button04.gif" alt="" />
